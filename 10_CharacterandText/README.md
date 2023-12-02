f4e2b89cfea6c66ef3322add47fbf4d4ed1a1eb3
SamMousaviES <sam.mousavi.engineering@gmail.com>
2023-12-02 12:28:44 +0200

    add 09 and 10 HWs

---
+++ 10_CharacterandText/README.md
@@ -0,0 +1,5 @@
+# Cpp Small Projects
+
+This repository is a place to store the homeworks of C++ Programming Course, Instructed by Daniel Gakwaya
+
+Hello
---
+++ 10_CharacterandText/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    char value = 42;
+
+    std::cout << "value: " << value << std::endl;
+    std::cout << "sizeof char: " << sizeof(value) << std::endl;
+
+    // bool red_light {true};
+    // bool green_light {false};
+
+    // if (red_light == true) { // or it can be if (red_light) {
+    //     std::cout << "STOP!" << std::endl;
+    // } else {
+    //     std::cout << "GO GO GO!" << std::endl;
+    // }
+    // std::cout << "sizeof boolean: " << sizeof(red_light) << std::endl;
+
+
+    
+    // std::cout << std::endl;
+    // std::cout << "red_light: " << red_light << std::endl;
+    // std::cout << std::boolalpha; // Foerce to show true and false in terminal
+    // std::cout << "green_light: " << green_light << std::endl;
+
+
+}
